70805e9cde4f9090eefddefda70495174bbe954a
Jusemon <juansmm@outlook.com>
2025-01-04 17:36:57 -0500

    Adding search function

--- src/features/home/HomeList.tsx
+++ src/features/home/HomeList.tsx
@@ -1,12 +1,15 @@
 import SearchBar from '../../components/SearchBar';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useAuth } from '../../hooks/use-auth';
 import { useNavigate } from 'react-router-dom';
 import useTelegram from '../../hooks/use-telegram';
 import { Api } from 'telegram';
-import { Flex, Image, Masonry, Text } from 'gestalt';
+import { Box, Flex, Image, Masonry, Text } from 'gestalt';
+import { IterMessagesParams } from 'telegram/client/messages';
 
 type MessageElement = {
+  id: number;
+  date: number;
   url: string;
   width: number;
   height: number;
@@ -28,12 +31,76 @@ function GridComponent({ data }: { data: MessageElement }) {
 }
 
 export default function HomeList() {
-  const LIMIT = 15;
+  const LIMIT = 50;
   const telegram = useTelegram();
   const { user } = useAuth();
   const navigate = useNavigate();
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
   const [searchValue, setSearchValue] = useState<string>();
   const [results, setResults] = useState<Array<MessageElement>>([]);
+
+  const loadItems = async (args: { from: number } | undefined) => {
+    if (!searchValue || !telegram) return;
+
+    const { from } = Object.assign({ from: 0 }, args);
+    console.log({ from });
+    const client = await telegram.getClient();
+    const request = {
+      search: searchValue,
+      filter: new Api.InputMessagesFilterPhotos(),
+      offsetId: results.length > 0 ? results.at(from - 1)?.id : 0,
+      limit: LIMIT,
+    };
+    // const messages = await client.getMessages(undefined, request); // Change undefined by channel id or entity
+  };
+
+  const performSearch = async (
+    args: { from: number } | undefined = undefined
+  ) => {
+    if (!searchValue || !telegram) return;
+    const { from } = Object.assign({ from: 0 }, args);
+    const client = await telegram.getClient();
+    const getMessagesProps: Partial<IterMessagesParams> = {
+      search: searchValue,
+      filter: new Api.InputMessagesFilterPhotos(),
+      ...(from > 0
+        ? { offsetDate: results.at(from - 1)?.date }
+        : { offsetDate: new Date().getTime() / 1000 }),
+      limit: LIMIT,
+    };
+
+    // const channels = await client.getEntity('@printergram');
+    // console.log({ channels });
+
+    const messages = await client.getMessages(undefined, getMessagesProps);
+    const messagesWithPhotosPromise = messages.map(async (message) => {
+      if (!message.photo) {
+        return null;
+      }
+
+      const photo = message.photo as unknown as {
+        id: number;
+        sizes: Array<Api.PhotoSize>;
+      };
+      const sizePos = photo.sizes.length - 2;
+      const size = photo.sizes[sizePos];
+      const photoBuffer = (await client.downloadMedia(message, {
+        thumb: sizePos,
+      })) as unknown as Buffer;
+      const data = {
+        id: message.id,
+        url: URL.createObjectURL(new Blob([photoBuffer])),
+        height: size.h,
+        width: size.w,
+        text: message.text,
+      };
+      return data;
+    });
+    const messagesWithPhotos = (
+      await Promise.all(messagesWithPhotosPromise)
+    ).filter((message) => message !== null) as Array<MessageElement>;
+    setResults(messagesWithPhotos);
+  };
 
   useEffect(() => {
     if (!user) {
@@ -46,44 +113,30 @@ export default function HomeList() {
       return;
     }
     console.log('search value has changed, searching...', { searchValue });
-
-    (async () => {
-      const client = await telegram.getClient();
-      const messages = await client.getMessages(undefined, {
-        search: searchValue,
-        filter: new Api.InputMessagesFilterPhotos(),
-        limit: LIMIT,
-      });
-      const promises = messages.map<Promise<MessageElement>>(
-        async (message) => {
-          const photo = message.photo as unknown as {
-            sizes: Array<Api.PhotoSize>;
-          };
-          const sizePos = photo.sizes.length - 2;
-          const size = photo.sizes[sizePos];
-          const photoBuffer = (await client.downloadMedia(message, {
-            thumb: sizePos,
-          })) as unknown as Buffer;
-          return {
-            url: URL.createObjectURL(new Blob([photoBuffer])),
-            height: size.h,
-            width: size.w,
-            text: message.text,
-          };
-        }
-      );
-      const data = await Promise.all(promises);
-      setResults(data);
-    })();
+    performSearch();
+    setResults([]);
   }, [searchValue, telegram]);
 
   return (
     <>
       <SearchBar searchValueHandler={setSearchValue} />;
-      <Masonry
-        items={results}
-        renderItem={({ data }) => <GridComponent data={data} />}
-      />
+      <Box
+        minHeight='calc(100vh - 130px)'
+        marginStart='auto'
+        marginEnd='auto'
+        ref={(el) => (scrollContainerRef.current = el)}
+        width='100%'
+      >
+        {scrollContainerRef.current && (
+          <Masonry
+            scrollContainer={() => scrollContainerRef.current!}
+            loadItems={loadItems}
+            virtualize={true}
+            items={results}
+            renderItem={({ data }) => <GridComponent data={data} />}
+          />
+        )}
+      </Box>
     </>
   );
 }
